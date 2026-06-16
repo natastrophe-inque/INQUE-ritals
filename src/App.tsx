6a4c5bcf4bd85e11e94a9e7c0283c020f0969501
natastrophe-inque <natashaczernienko@gmail.com>
2026-06-16 18:39:59 -0400

    Add App root with all landing page sections

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { Navigation } from '@/components/Navigation'
+import { Footer } from '@/components/Footer'
+import Hero from '@/components/Hero'
+import ScienceSection from '@/components/ScienceSection'
+import SkinTraumaSection from '@/components/SkinTraumaSection'
+import FormulaSection from '@/components/FormulaSection'
+import FounderSection from '@/components/FounderSection'
+import ArtistProgramSection from '@/components/ArtistProgramSection'
+import FinalCTASection from '@/components/FinalCTASection'
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-black text-bone antialiased">
+      <Navigation />
+      <main>
+        <Hero />
+        <ScienceSection />
+        <SkinTraumaSection />
+        <FormulaSection />
+        <FounderSection />
+        <ArtistProgramSection />
+        <FinalCTASection />
+      </main>
+      <Footer />
+    </div>
+  )
+}
